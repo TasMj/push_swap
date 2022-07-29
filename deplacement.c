2150a6ee2fe35ca189b0dde825450f56e37743c7
Tasnim MEJRI <tmejri@e1r5p16.clusters.42paris.fr>
2022-07-29 13:44:31 +0200

    revelation

--- deplacement.c
+++ deplacement.c
@@ -3,15 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   deplacement.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tas <tas@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 15:49:44 by tas               #+#    #+#             */
-/*   Updated: 2022/07/29 00:11:46 by tas              ###   ########.fr       */
+/*   Updated: 2022/07/29 13:22:27 by tmejri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// // swap les 2 premiers elts du haut de la stack A
-// void    sa(int a, int b)
+#include "push_swap.h"
+
+//fonction pour swap des content de 2 listes
+
+void    *swap(t_list *list)
+{
+    t_list *tmp;
+
+    tmp = malloc(sizeof(t_list));
+    if (!tmp)
+        return (0);
+	list = list->next;
+       
+    tmp->content = list->content;
+
+        
+        list_stack_a = list_stack_a->next;
+        tmp2->content = copy_content(list_stack_a);
+}
+
+// swap les 2 premiers elts du haut de la stack A
+void    sa()
+{
+    
+}
+
+
+
+
+
+
+
+
 
 // // swap les 2 premiers elts du haut de la stack B
 // void    sb(int a, int b)
@@ -23,41 +54,3 @@
 
 // void    swap(t_list stack) // marche pour A et B
 
-#include "push_swap.h"
-
-void    *copy_content(t_list *list_to_copy)
-{
-    t_list  *new_list;
-
-    new_list = malloc(sizeof(t_list));
-    if (!new_list)
-        return (NULL);
-    new_list->content = list_to_copy->content;
-}
-
-
-
-void	ft_lstiter(t_list *lst, void (*f)(void *))
-{
-	if (lst && f)
-	{
-		while (lst)
-		{
-			f(lst->content);
-			lst = lst->next;
-		}
-	}
-}
-
-void    *swap(t_list **list_stack_a)
-{
-    t_list *tmp;
-    t_list *tmp2;
-
-    while (list_stack_a)
-    {
-        tmp->content = copy_content(list_stack_a);
-        list_stack_a = list_stack_a->next;
-        tmp2->content = copy_content(list_stack_a);
-    }
-}
---
+++ trash.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trash.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/29 13:06:24 by tmejri            #+#    #+#             */
+/*   Updated: 2022/07/29 13:09:22 by tmejri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// void    *copy_content(t_list *list_to_copy)
+// {
+//     t_list  *new_list;
+
+//     new_list = malloc(sizeof(t_list));
+//     if (!new_list)
+//         return (NULL);
+//     new_list->content = list_to_copy->content;
+// }
+
+
+
