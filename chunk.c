607dd31235ddb0e55ed1211addbb09a7b37b5a84
Tasnim MEJRI <tmejri@e1r11p14.clusters.42paris.fr>
2022-10-25 17:55:06 +0200

    start te chunk sort

--- chunk.c
+++ chunk.c
@@ -6,11 +6,11 @@
 /*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 11:33:20 by tmejri            #+#    #+#             */
-/*   Updated: 2022/10/18 11:33:21 by tmejri           ###   ########.fr       */
+/*   Updated: 2022/10/25 17:00:42 by tmejri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "push_swap.h"
+#include "push_swap.h"
 
 // // 1) en fonction du nb d'elt separer en plusieurs partie
 // // 2) trier ds un tab
@@ -23,31 +23,121 @@
 // // si c'est au dessus de la premiere partie on pb et on trie
 
 
-// // 1) en fonction du nb d'elt separer en plusieurs partie
-// int	size_block(t_list **list_a)
-// {
-//     int	size_list;
+// 1) en fonction du nb d'elt separer en plusieurs partie
+int	size_block(t_list **list_a)
+{
+    int	size_list;
 	
-//     size_list = ft_lstsize(list_a);
-//     if (size_list < 51)
-// 		return (size_list / 2);
-//     if (size_list < 101)
-// 		return (size_list / 4);
-//     if (size_list < 200)
-// 		return (size_list / 4);
+    size_list = ft_lstsize(*list_a);
+    if (size_list <= 50)
+		return (size_list / 2);
+    else if (size_list <= 100)
+		return (size_list / 4);
+    else if (size_list <= 200)
+		return (size_list / 6);
+    else
+    	return (size_list / 10);
+}
 
-	
+
+
+/* Creation d'un block */
+// void	creat_block(t_list **list_a, t_list **list_b, int *tab)
+// {
+// 	int size_of_block;
+// 	int	i;
+//     t_list  *tmp;
+
+// 	i = 0;
+//     tmp = *list_a;
+// 	size_of_block = size_block(list_a);
+//     tab = tab_to_sort(list_a);
+//     tab = tab_sorted(tab, list_a);
+// 	while(size_of_block)
+// 	{
+// 		while (*list_a)
+// 		{
+// 			while (tab[i])
+// 			{
+// 				if ((*list_a)->content == tab[i])
+// 					pb(list_b, list_a);
+//                 else
+//                     i++;
+// 			}
+//             i = 0;
+// 		}
+// 	    size_of_block--;
+// 	}
+//     *list_a = tmp;
 // }
 
+void	sort_by_block(t_list **list_a, t_list **list_b)
+{
+	int		*tab;
+
+	tab = tab_to_sort(list_a);
+	tab = tab_sorted(tab, list_a);
+	index_tab(list_a, tab);
+	
+	list_b = creat_block(list_a, list_b);
+	if (size_list_a == 6)
+		sort_for_3(list_a, (*list_a)->next, (*list_a)->next->next);
+	if (size_list_a == 7)
+		sort_for_4(list_a, list_b);
+	else
+	{
+		stack_to_5(list_a, list_b);
+		sort_for_5(list_a, list_b);
+	}
+	sort_in_stack_a(list_a, list_b);
+}
+
+int main(int argc, char **argv)
+{
+	t_list **list;
+	t_list **list_b;
+    int    *tab;
+    int i = 0;
+
+	list = attribution_arg(argc, argv, 2);
+	list_b = malloc(sizeof(t_list));
+	// bubble_sort(list, list_b);
+    tab = tab_to_sort(list);
+    tab = tab_sorted(tab, list);
+	index_tab(list, tab);
+    // creat_block(list, list_b, tab);
+   	printf("TAAAAAAAAAAAAAAAAB\n\n");
+	while (tab[i])
+	{
+		printf("[%d]\n", tab[i]);
+		i++;
+	}
+
+	printf("/////////////// LISTE A /////////////////\n\n");
+	while (*list)
+	{
+		printf("[%d]\n", (*list)->content);
+		(*list) = (*list)->next;
+	}
+	printf("\n////////////// LISTE B //////////////////\n");
+	while (*list_b)
+	{
+		printf("[%d]\n", (*list_b)->content);
+		(*list_b) = (*list_b)->next;
+	}
+	printf("////////////////////////////////\n");
+	
+}
+
 // // 2) trier ds un tab ds l'ordre décroissant
-// void	*tab_to_sort(t_list **list)
+// void	*tab_to_sort_dec(t_list **list)
 // {
 // 	int		size;
 // 	int		*tab_int;
 // 	t_list	*tmp;
-// 	int		i;
+// 	// int		i;
 
-// 	i = 0;
+// 	// i = 0;
 // 	size = ft_lstsize(*list);
 // 	tab_int = (int *)malloc(sizeof(int) * size);
 // 	tmp = *list;
@@ -67,50 +157,4 @@
 // 	int	index;
 
 // 	index = 
-// }
-
-// void	creat_block(t_list **list_a, t_list **list_b, int *tab_sorted)
-// {
-// 	int size_block;
-// 	int	i;
-
-// 	i = 0;
-// 	size_block = size_block(list_a);
-// 	while(size_block)
-// 	{
-// 		while (tab[i])
-// 		{
-// 			while (*list_a)
-// 			{
-// 				if ((*list_a)->content == tab[i])
-					
-// 				(*list_a) = (*list_a)->next;
-// 			}
-// 		}
-// 	}
-// 	size_block--;
-// }
-
-
-
-// void	sort_by_block(t_list **list_a, t_list **list_b)
-// {
-// 	int		*tab;
-
-// 	tab = tab_to_sort(list_a);
-// 	tab = tab_sorted(tab, list_a);
-// 	index_tab(list_a, tab);
-	
-	
-// 	list_b = seperate_by_mediane(list_a, list_b);
-// 	if (size_list_a == 6)
-// 		sort_for_3(list_a, (*list_a)->next, (*list_a)->next->next);
-// 	if (size_list_a == 7)
-// 		sort_for_4(list_a, list_b);
-// 	else
-// 	{
-// 		stack_to_5(list_a, list_b);
-// 		sort_for_5(list_a, list_b);
-// 	}
-// 	sort_in_stack_a(list_a, list_b);
 // }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 17:28:11 by tmejri            #+#    #+#             */
-/*   Updated: 2022/10/18 18:11:09 by tmejri           ###   ########.fr       */
+/*   Updated: 2022/10/25 16:59:21 by tmejri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,28 @@ void	direct_sort(int size_list, t_list **list_a, t_list **list_b)
 		sort_for_hundread(list_a, list_b, size_list);
 }
 
-int	main(int argc, char **argv)
-{
-	t_list	**list_a;
-	t_list	**list_b;
-	int		size_list;
+// int	main(int argc, char **argv)
+// {
+// 	t_list	**list_a;
+// 	t_list	**list_b;
+// 	int		size_list;
 
-	if (argc < 2)
-		return (1);
-	list_b = malloc(sizeof(t_list));
-	if (!list_b)
-		return (1);
-	list_b[0] = NULL;
-	if (check_int(argc, argv))
-		return (error_free_ret(*list_b));
-	else
-	{
-		list_a = attribution_arg(argc, argv, 2);
-		size_list = ft_lstsize(*list_a);
-		if (check_doublon(list_a) != 0)
-			return (error_free_ret(*list_b));
-		else if (size_list == 1)
-			return (0);
-		direct_sort(size_list, list_a, list_b);
-	}
-}
+// 	if (argc < 2)
+// 		return (1);
+// 	list_b = malloc(sizeof(t_list));
+// 	if (!list_b)
+// 		return (1);
+// 	list_b[0] = NULL;
+// 	if (check_int(argc, argv))
+// 		return (error_free_ret(*list_b));
+// 	else
+// 	{
+// 		list_a = attribution_arg(argc, argv, 2);
+// 		size_list = ft_lstsize(*list_a);
+// 		if (check_doublon(list_a) != 0)
+// 			return (error_free_ret(*list_b));
+// 		else if (size_list == 1)
+// 			return (0);
+// 		direct_sort(size_list, list_a, list_b);
+// 	}
+// }
