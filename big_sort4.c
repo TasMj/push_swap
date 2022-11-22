d5be00c30a28c2be5a59951ffb87855e49f120c2
Tasnim MEJRI <tmejri@e3r2p13.clusters.42paris.fr>
2022-11-22 20:25:20 +0100

    checking 1 by 1

--- big_sort4.c
+++ big_sort4.c
@@ -6,7 +6,7 @@
 /*   By: tmejri <tmejri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:34:16 by tmejri            #+#    #+#             */
-/*   Updated: 2022/11/21 20:17:43 by tmejri           ###   ########.fr       */
+/*   Updated: 2022/11/22 20:23:54 by tmejri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,65 +104,170 @@ t_list	*index_middle(t_list **list_a, int middle)
 	return (midd);
 }
 
+int	biggest(t_list	**list_a)
+{
+	int		max;
+	t_list	*tmp;
+
+	tmp = *list_a;
+	max = (*list_a)->index;
+	(*list_a) = (*list_a)->next;
+	while ((*list_a))
+	{
+		if ((*list_a)->index > max)
+			max = (*list_a)->index;
+		(*list_a) = (*list_a)->next;
+	}
+	*list_a = tmp;
+	return (max);
+}
+
 int find_rank_top(t_list **list_a, t_list **list_b, int middle)
 {
 	int	c;
 	int	size_a;
+	int max_a;
 	t_list	*last;
 	t_list	*midd;
 
 	c = 0;
+	max_a = biggest(list_a);
 	size_a = ft_lstsize(*list_a);
 	last = last_elt(list_a, size_a);
-	midd = index_middle(list_a, middle);
-	if ((*list_b)->index < (*list_a)->index)
+	midd = last_elt(list_a, middle);
+	printf("middle: %d\n", middle);
+	printf("B: %d\n", (*list_b)->index);
+	printf("last index: %d\n", last->index);
+	printf("A: %d\n", (*list_a)->index);
+	printf("MAX: %d\n", max_a);
+	printf("*******************************\n");
+	if ((((*list_b)->index < (*list_a)->index) && ((*list_b)->index > last->index)) && (((*list_b)->index < (*list_a)->index) && ((*list_a)->index < max_a)))
 	{
-		if (((*list_b)->index < (*list_a)->index) && ((*list_b)->index > last->index)) //premier elt
-			return (0);
-		else if ((*list_b)->index < midd->index)
-			return (-1);
-		else
+		printf("4444444444444444444\n");
+		return (0);
+	}
+	else if (((*list_b)->index < (*list_a)->index) && ((*list_b)->index > midd->index))
+	{
+		printf("555555555555555555555\n");
+		
+		while ((*list_a) && middle > 0)
 		{
-			while (((*list_b)->index < (*list_a)->index) && middle > 0)
+			while ((*list_b)->index < (*list_a)->index)
 			{
 				(*list_a) = (*list_a)->next;
 				c++;
 				middle--;
 			}
-			if (c == 0)
-				return (-1);
+			while ((*list_b)->index > (*list_a)->index)
+			{
+				(*list_a) = (*list_a)->next;
+				c++;
+				middle--;
+			}
 			return (c);
 		}
+		return (c);
 	}
-	printf("ON EST BON");
-	return (-1);
-}
-
-int find_rank_down(t_list **list_a, t_list **list_b, int middle)
-{
-	int	c;
-
-	c = 0;
-	if ((*list_b)->index > (*list_a)->index)
+	else if ((*list_b)->index > (*list_a)->index)
 	{
-		while ((*list_a) && middle > 0)
+		printf("66666666666666666666\n");
+		while ((middle > 0) && (*list_a))
 		{
-			if ((*list_b)->index > (*list_a)->index)
+			if (((*list_b)->index > (*list_a)->index))
 			{
-				printf("MIDDLE: %d\n", middle);
-				printf("JPP\n");
-				printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAA: %d\n", (*list_a)->index);
 				(*list_a) = (*list_a)->next;
 				c++;
 			}
 			middle--;
 		}
+		if (middle == 0)
+			return (-1);
 		return (c);
 	}
+	
 	return (-1);
 }
 
 
+
+
+int find_rank_down(t_list **list_a, t_list **list_b, int middle, t_list	*last_top, t_list *tmp)
+{
+	int		c;
+	int		max_a;
+	int		size_a;
+	t_list	*last;
+
+	c = 0;
+	size_a = ft_lstsize(*list_a);
+	last = last_elt(list_a, size_a);
+	max_a = biggest(list_a);
+	printf("middle: %d\n", middle);
+	printf("first index: %d\n", tmp->index);
+	printf("B: %d\n", (*list_b)->index);
+	printf("last index: %d\n", last->index);
+	printf("A: %d\n", (*list_a)->index);
+	printf("MAX: %d\n", max_a);
+	if (((*list_b)->index > last->index))
+	{
+		printf("11111111111111111111\n");
+		return (middle);
+	}
+	else if (((*list_b)->index < (*list_a)->index) && ((*list_b)->index > last_top->index))
+	{
+		printf("22222222222222222222\n");	
+		return (0);
+	}
+	else if (((*list_b)->index < (*list_a)->index) ) // et inf au + grand
+	{                                                // && (*list_b)->index < (*list_a)->index && (*list_a)->index < max
+		
+	printf("MAX: %d\n", max_a);
+		while ((middle > 0) && (*list_a))
+		{
+			while (((*list_b)->index < (*list_a)->index) && ((*list_a)->index < max_a))
+			{
+		printf("333333333333333333\n");
+				printf("index: %d\n", (*list_a)->index);
+				(*list_a) = (*list_a)->next;
+				c++;
+				middle--;
+			}
+			if ((*list_a)->index == max_a)
+				return (c + 1);
+			while ((*list_b)->index > (*list_a)->index)
+			{
+				(*list_a) = (*list_a)->next;
+				c++;
+				middle--;
+			}
+			if (middle == 0)
+				return (-1);
+			return (c);
+		}
+		return (c);
+	}
+	else if ((*list_b)->index > (*list_a)->index)
+	{
+		while ((middle > 0) && (*list_a))
+		{
+		printf("4444444444444444444444\n");
+			if (((*list_b)->index > (*list_a)->index))
+			{
+				(*list_a) = (*list_a)->next;
+				c++;
+			}
+			middle--;
+		}
+		printf("MIDDLE: %d\n", middle);
+		if (middle == 0)
+			return (-1);
+// 
+		// if ((*list_b)->index > (*list_a)->index)
+			// return (-1);
+		return (c);
+	}
+	return (-1);
+}
 
 int	from_top(t_list **list_a, t_list **list_b)
 {
@@ -177,10 +282,12 @@ int	from_top(t_list **list_a, t_list **list_b)
 	middle = middle_size(list_a);
 	keep_middle = middle;
 	c = find_rank_top(list_a, list_b, middle);
+	printf("c: %d\n", c);
 	if (c == keep_middle)
 		r = -1;
 	else
 		r = c;
+	printf("r: %d\n", r);
 	*list_a = tmp;
 	return (r);
 }
@@ -199,7 +306,7 @@ int down_even_odd(int keep_middle, int size_a, int c)
 	}
 	else
 	{
-		if (c == keep_middle)
+		if (c == (keep_middle - 1))
 			r = 0;
 		else
 			r = (keep_middle - c);
@@ -215,24 +322,31 @@ int	from_down(t_list **list_a, t_list **list_b)
 	int		c;
 	int		r;
 	t_list	*tmp;
+	t_list	*last_top;
 
 	r = 0;
 	tmp = *list_a;
+	last_top = *list_a;
 	size_a = ft_lstsize(*list_a);
 	middle = middle_size(list_a);
 	keep_middle = middle;
-	while (middle > 0)
+	while (middle > 1)
 	{
 		(*list_a) = (*list_a)->next;
+		last_top = (*list_a);
 		middle--;
 	}
+	(*list_a) = (*list_a)->next;
 	if (size_a % 2 == 0)
 		middle = keep_middle;
 	else
 		middle = keep_middle - 1;
-	c = find_rank_down(list_a, list_b, middle);
+	c = find_rank_down(list_a, list_b, middle, last_top, tmp);
 	printf("c: %d\n", c);
-	r = down_even_odd(keep_middle, size_a, c);
+	if (c == -1)
+		r = -1;
+	else
+		r = down_even_odd(keep_middle, size_a, c);
 	printf("r: %d\n", r);
 	*list_a = tmp;
 	return (r);
@@ -247,13 +361,14 @@ void	sort_in_stack_a(t_list **list_a, t_list **list_b)
 	final_size = ft_lstsize(*list_a) + ft_lstsize(*list_b);
 	while (ft_lstsize(*list_a) != final_size)
 	{
-		print_list(list_a, list_b);
+		// print_list(list_a, list_b);
 		top = from_top(list_a, list_b);
-		// printf("*************************************************************************\n");
 		down = from_down(list_a, list_b);
+		printf("TOP: %d   DOWN: %d\n", top, down);
 		if (down == 0 || (down < top && down != -1) || top == -1)
 			move_from_down(list_a, list_b, down);
 		else
 			move_from_top(list_a, list_b, top);
+		print_list(list_a, list_b);
 	}
 }
